d793fa7e846789da84eef84beca892703d19b767
Luis Noriega <noriegajorge00@gmail.com>
2024-09-30 14:22:38 -0600

    Se subio el proyecto login a la nube

---
+++ main.js
@@ -0,0 +1,12 @@
+let usuario = prompt("ingres su usuario")
+let cotrasena = prompt("ingrese su contraseña")
+
+
+if(usuario == "administrador" && cotrasena == "admin123"){
+    document.write("Bienvenido")
+} else{
+    document.write("valores no validos")
+}
+
+
+
